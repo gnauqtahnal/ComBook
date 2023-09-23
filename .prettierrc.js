e11119078abd4541de2b3609bbfa78200e1010bf
GnauqTahnAl <gnauq.tahn.al@gmail.com>
2023-09-23 12:35:32 +0700

    prettier sort imports setup

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,13 @@
 /** @type {import("prettier").Config} */
 const config = {
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
   trailingComma: 'es5',
   tabWidth: 2,
   semi: false,
   singleQuote: true,
+  importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
 }
 
 module.exports = config
