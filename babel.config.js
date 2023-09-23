ca159fdc08c57667ccc23b5b5eabd611354551e2
GnauqTahnAl <gnauq.tahn.al@gmail.com>
2023-09-23 13:00:00 +0700

    gluestack-ui setup

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
-module.exports = function(api) {
-  api.cache(true);
+const gluestackStyleResolver = require('@gluestack-style/babel-plugin-styled-resolver')
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [gluestackStyleResolver],
+  }
+}
